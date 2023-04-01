9cf8631675e5eac57557aa8523dca8d1bc9f509a
Chenxuan Wang <cxleoo9@gmail.com>
2023-04-01 00:25:55 -0600

    update

---
+++ src/components/ListEmployeeComponent.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class ListEmployeeComponent extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state= {
+            employees: []
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default ListEmployeeComponent;
